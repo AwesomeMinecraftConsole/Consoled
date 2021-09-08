b39b335f0cb2156d7decb42294c3a04fde350eeb
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-08 23:24:08 +0900

    :art: 環境変数に変更

--- src/main/kotlin/com/uramnoil/awesome_minecraft_console/consoled/Main.kt
+++ src/main/kotlin/com/uramnoil/awesome_minecraft_console/consoled/Main.kt
@@ -3,7 +3,7 @@ package com.uramnoil.awesome_minecraft_console.consoled
 import kotlinx.coroutines.coroutineScope
 
 suspend fun main(): Unit = coroutineScope {
-    Weaver("127.0.0.1", 50052).use {
+    Weaver(System.getenv("WEAVER_SERVER_HOST"), System.getenv("WEAVER_SERVER_PORT").toInt()).use {
         it.start()
         it.await()
     }
